82b5216c43231df441761054cd4bf68f6f76ca52
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-03-25 22:10:17 -0500

    added new files

--- src/main.py
+++ src/main.py
@@ -1,11 +1,1 @@
-# import the class from the textnode file
-from textnode import TextNode
-
-
-def main():
-    # create node with dummy data
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
-
-
-main()
+print("hello)
