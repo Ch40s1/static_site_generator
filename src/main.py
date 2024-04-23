f7c1f809804cf60d19d92d11287370cbf8dfe306
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2024-04-22 23:28:01 -0500

    main copies static content to public

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
-from textnode import TextNode
+import os
+import shutil
 
 
 def main():
+    if os.path.exists("../public/"):
+        shutil.rmtree("../public/")
 
-    node = TextNode("hello", "based", "google")
-    print(node)
+    os.mkdir("./public/")
+
+    src_dir = "./static/"
+    dest_dir = "./public/"
+
+    def copy_directory(src_dir, dest_dir):
+        for entry in os.listdir(src_dir):
+            src_entry = os.path.join(src_dir, entry)
+            dest_entry = os.path.join(dest_dir, entry)
+
+            if os.path.isfile(src_entry):
+                shutil.copy(src_entry, dest_entry)
+
+            elif os.path.isdir(src_entry):
+                shutil.copytree(src_entry, dest_entry)
+
+    copy_directory(src_dir, dest_dir)
 
 
 main()
